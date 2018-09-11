dff5d40852890fea15d1bc1171f8e8e1cb3b7a16
olsonso <olson1sonja@gmail.com>
2018-09-10 18:21:37 -0700

    first commit with structure set up

---
+++ src/Components/MainContainer/mainContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MainContainer extends Component {
+  render(){
+    return(
+      <h1> Here is where main container will be / graphs </h1>
+    )
+  }
+}
+
+export default MainContainer;
